34cc64fcea505959127fcb5c33d4fcc8b8963a91
koppurohithkumar <koppurohithkumar37@gmail.com>
2024-11-21 19:36:16 +0530

    carousel line17 warning

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -14,7 +14,7 @@ function Carousel({
     };
     const SlideInterval = setInterval(next, autoSlideInterval);
     return () => clearInterval(SlideInterval);
-  }, [autoSlide, autoSlideInterval]);
+  }, [autoSlide, autoSlideInterval, images.length]);
 
   return (
     <div className="overflow-hidden relative">
